31079b71f3ea11054a94d231a875ada63cf6703e
Joss-Steward <joss.steward@gmail.com>
2016-03-04 23:19:22 -0500

    Comments

--- src/ConvertingMachine.java
+++ src/ConvertingMachine.java
@@ -30,32 +30,46 @@ public class ConvertingMachine
 
 	public double parse(String text)
 	{
+		// Tracks the current state
 		State currentState = State.START;
+		
+		// Tracks the current results. Initialize to starting values
 		InterimResult interim = new InterimResult(0, 1, 0);
 		
+		// For each character, find the matching edge and execute that action.
 		for(Character character : text.toCharArray()){
+			// Find the edge
 			Edge nextEdge = searchForEdge(currentState, character);
+			
+			// Execute the action
 			interim = nextEdge.action.execute(interim, character);
+			
+			// Advance to the next state
 			currentState = nextEdge.nextState;
 		}
 		
+		// Return the final result.
 		return interim.getS() * interim.getV();
 	}
 
 	private Edge searchForEdge(State currentState, char ch)
 	{
 		Edge nextEdge = null;
-		
+
+		// Look through all the edges
 		for(Edge e : machine){
+			// If this edge matches, then set it
 			if(e.currentState == currentState && e.inputVerifier.meetsCriteria(ch)){
 				nextEdge = e;
 			}
 		}
 		
+		// If we didn't find an edge, throw an exception
 		if(nextEdge == null){
 			throw new NumberFormatException();
 		}
 		
+		// Return the edge we found
 		return nextEdge;
 	}
 
